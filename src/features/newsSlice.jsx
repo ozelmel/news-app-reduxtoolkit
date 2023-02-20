8983b56052da6ed523954c0b08dc63280614fc7f
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-20 22:15:39 +0300

    v1

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,17 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  user: "",
+  newsList: [],
+  loading: true,
 };
+
+//? state'lerin API gibi async kaynaklardan gelen verilere göre güncellenmesi gerekebilir.
+//? async işlem tamamlandıktan soru state güncellenmeli
+//? gönderilen api isteği ile dogrudan state güncellenmemelidir.
+//?Bunun için bir arabirim kullanılıyor, bunun adına middleware denilir. Redux-toolkit default olarak Thunk kullanmaktadır.
+//* Thunk'un amacı reducer a islenmiş sonucları gondermeden once gecikmeli asenkron islemlerin yurutulmesini saglamaktır.
+
+const API_KEY = "7df8731b556a4966b0412e084f455423";
+//news/getNews action-type ismi   ****async
+export const getNews = createAsyncThunk("news/getNews", async () => {
+  const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
+  try {
+      const { data } = await axios.get(url);
+      console.log(data.articles)
+      
+    return data.articles;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const newsSlice = createSlice({
   name: "news",
   initialState,
   reducers: {
-    
+    clearNewsList: (state) => {
+      state.newsList = [];
+    },
+  },
+  extraReducers:  {
+    [getNews.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getNews.fulfilled]: (state, {payload}) => {
+      state.loading = false;
+      state.newsList = payload;
+    },
+    [getNews.rejected]: (state) => {
+      state.loading = false;
+    },
   },
 });
 
-// export const { setUser, clearUser } = authSlice.actions;
+//! baska slice'lardaki tanimlanan action'lara cevap vermek
+//! bilhassa createAsyncThunk tarafından oluşturulan action'lara
+//! cevap vermek için kullanılır.
+export const { clearNewsList } = newsSlice.actions;
 
 export default newsSlice.reducer;
---
+++ src/pages/News.jsx
@@ -0,0 +1,88 @@
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardMedia } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import loadingGif from "../assets/loading.gif";
+import { useEffect } from "react";
+import { clearNewsList, getNews } from "../features/newsSlice";
+
+const News = () => {
+  const dispatch = useDispatch();
+  const { newsList, loading } = useSelector((state) => state.news);
+  console.log(loading);
+
+  useEffect(() => {
+    dispatch(getNews());
+    return () => {
+       dispatch(clearNewsList())
+     }
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="100vh"
+        >
+          <img src={loadingGif} alt="gif" width="90%" height="800px" />
+        </Box>
+      )}
+      {!loading && (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-evenly"
+          flexWrap="wrap"
+        >
+          {newsList?.map((item, index) => (
+            <Card
+              sx={{
+                maxWidth: 345,
+                m: 5,
+                maxHeight: 600,
+              }}
+              key={index}
+            >
+              <CardMedia
+                component="img"
+                height="250"
+                image={
+                  item?.urlToImage ??
+                  "https://ichef.bbci.co.uk/news/976/cpsprodpb/5A8B/production/_122497132_tesla.png"
+                }
+                alt="img"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {" "}
+                  {item?.title ??
+                    "Tesla disables gaming while driving feature"}{" "}
+                </Typography>{" "}
+                <Typography variant="body2" color="text.secondary">
+                  {" "}
+                  {item?.content ??
+                    "It follows an inquiry into Passenger Play, which allowed games to be played while a car was moving."}{" "}
+                </Typography>{" "}
+              </CardContent>{" "}
+              <CardActions>
+                <Button size="small"> Share </Button>{" "}
+                <Button size="small" href={item?.url} target="_blank">
+                  Detail{" "}
+                </Button>{" "}
+              </CardActions>{" "}
+            </Card>
+          ))}{" "}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default News;
